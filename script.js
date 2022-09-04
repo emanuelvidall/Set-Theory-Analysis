e22af9e30987da28ff5e62435ad8dd7988e42696
synthaxme <emanuelvidalrm@gmail.com>
2022-09-04 20:53:37 -0300

    added chartjs with json data successfully

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let ArrayAlunos = [];
 
 async function fetchData(){
-    const url = 'alunoss.json';
+    const url = 'alunosformated.json';
     const response = await fetch(url);
     //wait request complete
     const datapoints = await response.json();
@@ -9,31 +8,69 @@ async function fetchData(){
     for (let i = 0; i < datapoints.alunos.length; i++) {
         ArrayAlunos.push(datapoints.alunos[i].nome);
     }
-    return ArrayAlunos;
-}
 
-    console.log(ArrayAlunos);
+    const url2 = 'dengueformated.json';
+    const response2 = await fetch(url2);
+    //wait request complete
+    const datapoints2 = await response2.json();
+    DengueArray = [];
+        for (let i = 0; i < datapoints2.dengue.length; i++) {
+        DengueArray.push(datapoints2.dengue[i].nome);
+    }
 
-const data = ChartVenn.extractSets(
-    [
-        {
-            label: 'Alunos',
-            // values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        },
-    ]
-);
+    console.log(DengueArray);
+
+    const data = ChartVenn.extractSets(
+        [
+            {
+                label: 'Alunos',
+                values: ArrayAlunos,
+            },
+                    {
+                label: 'Dengue',
+                values: DengueArray,
+            },
+        ]
+    );
     // config 
     const config = {
-    type: 'venn',
-    data,
-    options: {
-    }
-};
+        type: 'venn',
+        data,
+        options: {
+            plugins: {
+            title: {
+                display: true,
+                text: 'Questão 1',
+            },
+            legend: {
+                display: false,
+            },
+            },
+            borderColor: [
+                'rgba(255, 26, 104, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(0, 0, 0, 1)'
+            ],
+            backgroundColor: [
+                'rgba(255, 26, 104, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(0, 0, 0, 0.2)'
+            ],
+        }
+    }; 
     // render init block
     const myChart = new Chart(
     document.getElementById('myChart'),
     config
     );
+}
 
 fetchData();
