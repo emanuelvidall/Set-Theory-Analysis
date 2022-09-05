e01875e911f909bcc0caa13f3ee8582279a2f804
synthaxme <emanuelvidalrm@gmail.com>
2022-09-05 01:52:06 -0300

    added second chart and script config

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 async function fetchData(){
     const url = 'alunosformated.json';
     const response = await fetch(url);
-    //wait request complete
     const datapoints = await response.json();
     ArrayAlunos = [];
     for (let i = 0; i < datapoints.alunos.length; i++) {
@@ -11,14 +10,23 @@ async function fetchData(){
 
     const url2 = 'dengueformated.json';
     const response2 = await fetch(url2);
-    //wait request complete
     const datapoints2 = await response2.json();
     DengueArray = [];
         for (let i = 0; i < datapoints2.dengue.length; i++) {
         DengueArray.push(datapoints2.dengue[i].nome);
     }
 
+    const url3 = 'onibusformated.json';
+    const response3 = await fetch(url3);
+    const datapoints3 = await response3.json();
+    OnibusArray = [];
+        for (let i = 0; i < datapoints3.onibus.length; i++) {
+        OnibusArray.push(datapoints3.onibus[i].nome);
+    }
+
     console.log(DengueArray);
+    console.log(ArrayAlunos);
+    console.log(OnibusArray);
 
     const data = ChartVenn.extractSets(
         [
@@ -32,6 +40,7 @@ async function fetchData(){
             },
         ]
     );
+
     // config 
     const config = {
         type: 'venn',
@@ -70,6 +79,58 @@ async function fetchData(){
     const myChart = new Chart(
     document.getElementById('myChart'),
     config
+    );
+
+        const data2 = ChartVenn.extractSets(
+        [
+            {
+                label: 'Onibus',
+                values: OnibusArray,
+            },
+                    {
+                label: 'Dengue',
+                values: DengueArray,
+            },
+        ]
+    );
+
+    const config2 = {
+        type: 'venn',
+        data: data2,
+        options: {
+            plugins: {
+            title: {
+                display: true,
+                text: 'Questão 3',
+            },
+            legend: {
+                display: false,
+            },
+            },
+            borderColor: [
+                'rgba(255, 26, 104, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(0, 0, 0, 1)'
+            ],
+            backgroundColor: [
+                'rgba(255, 26, 104, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(0, 0, 0, 0.2)'
+            ],
+        }
+    }; 
+
+    const myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config2
     );
 
 }
